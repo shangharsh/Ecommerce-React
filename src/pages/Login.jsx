4c6141936f232b1cdd9414d0f80fd5506abdddea
Shangharsh Adhikari <san.adh1996@gmail.com>
2024-05-18 17:41:02 +0545

    feat: Login and Register Page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,76 @@
+import { Card } from "react-bootstrap"
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Row from 'react-bootstrap/Row';
+import { Link } from "react-router-dom";
 
 const Login = () => {
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
   return (
-    <div>Login</div>
+    <div>
+        <h4 className="text-center mt-3">Please Fill The Form To Login.</h4>
+      <Card className="container mt-4 w-50">
+      <Form noValidate onSubmit={handleSubmit}>
+      <Row className="mb-3">
+        <Form.Group controlId="validationCustomUsername">
+          <Form.Label>Email</Form.Label>
+          <InputGroup hasValidation>
+            <InputGroup.Text id="inputGroupUsername">@</InputGroup.Text>
+            <Form.Control
+              type="text"
+              placeholder="Email"
+              aria-describedby="inputGroupPrepend"
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              Please Type Email.
+            </Form.Control.Feedback>
+          </InputGroup>
+        </Form.Group>
+        <Form.Group controlId="validationCustomPassword">
+          <Form.Label>Password</Form.Label>
+          <InputGroup hasValidation>
+            <InputGroup.Text id="inputGroupPassword">p</InputGroup.Text>
+            <Form.Control
+              type="text"
+              placeholder="Password"
+              aria-describedby="inputGroupPrepend"
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              Please Type password.
+            </Form.Control.Feedback>
+          </InputGroup>
+        </Form.Group>
+        </Row>
+      
+      <Row className="mb-3">
+        <Form.Group className="d-flex justify-content-center align-items-center">
+          <Button className="w-50" type="submit">LOGIN</Button>
+        </Form.Group>
+      </Row>
+      <Row className="mb-3">
+        <Form.Group className="d-flex justify-content-center align-items-center">
+          <p>Don't have a account? <Link to='/register'>Register Here</Link></p>
+        </Form.Group>
+      </Row>
+    </Form>
+
+      </Card>
+    </div>
   )
 }
 
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,98 @@
-import React from 'react'
+import { Card } from "react-bootstrap"
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Row from 'react-bootstrap/Row';
+import { Link } from "react-router-dom";
 
 const Register = () => {
+const [validated, setValidated] = useState(false);
+
+const handleSubmit = (event) => {
+  const form = event.currentTarget;
+  if (form.checkValidity() === false) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  setValidated(true);
+};
+
   return (
-    <div>Register</div>
+    <div>
+      <h4 className="text-center mt-3">Please Fill the form to Register</h4>
+      <Card className="container mt-4 w-75">
+      <Form noValidate onSubmit={handleSubmit}>
+      <Row className="mb-3">
+        <Form.Group as={Col} md="6" controlId="validationCustom01">
+          <Form.Label>First name</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            placeholder="First name"
+            defaultValue="Mark"
+          />
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group as={Col} md="6" controlId="validationCustom02">
+          <Form.Label>Last name</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            placeholder="Last name"
+            defaultValue="Otto"
+          />
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+        </Form.Group>
+      </Row>
+      <Row className="mb-3">
+        <Form.Group as={Col} md="6" controlId="validationCustomUsername">
+          <Form.Label>Username</Form.Label>
+          <InputGroup hasValidation>
+            <InputGroup.Text id="inputGroupUsername">@</InputGroup.Text>
+            <Form.Control
+              type="text"
+              placeholder="Email"
+              aria-describedby="inputGroupPrepend"
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              Please choose a username.
+            </Form.Control.Feedback>
+          </InputGroup>
+        </Form.Group>
+        <Form.Group as={Col} md="6" controlId="validationCustomPassword">
+          <Form.Label>Password</Form.Label>
+          <InputGroup hasValidation>
+            <InputGroup.Text id="inputGroupPassword">p</InputGroup.Text>
+            <Form.Control
+              type="text"
+              placeholder="Password"
+              aria-describedby="inputGroupPrepend"
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              Please choose a password.
+            </Form.Control.Feedback>
+          </InputGroup>
+        </Form.Group>
+        </Row>
+      
+        <Row className="mb-3">
+          <Form.Group className="d-flex justify-content-center align-items-center">
+            <Button className="w-50" type="submit">SIGN UP</Button>
+          </Form.Group>
+        </Row>
+        <Row className="mb-3">
+        <Form.Group className="d-flex justify-content-center align-items-center">
+          <p>Already Registered? <Link to='/login'>Login Here</Link></p>
+        </Form.Group>
+      </Row>
+    </Form>
+      </Card>
+    </div>
   )
 }
 
